812fd517e99ef9ec4e7cb848b76e7d4364c1013c
xtfocus <34581986+xtfocus@users.noreply.github.com>
2019-08-24 15:17:22 +1000

    Create vimPackages.sh
    for coding python in vim

---
+++ vimPackages.sh
@@ -0,0 +1,17 @@
+cd ~/.vim/bundle &&
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/msanders/snipmate.vim.git
+
+https://github.com/google/yapf.git
+git clone https://github.com/vim-scripts/pep8.git
+git clone https://github.com/tmhedberg/SimpylFold.git
+
+git clone https://github.com/vim-scripts/The-NERD-tree.git
+
+# YCM is kinda heavy, and google makes it. Use jedi
+# git clone https://github.com/ycm-core/YouCompleteMe.git
+#cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
+
+
+git clone --recursive https://github.com/davidhalter/jedi-vim.git
+git clone https://github.com/kien/ctrlp.vim.git
